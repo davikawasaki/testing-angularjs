6ac16e1e9e28d3cd6c0eaa456e8dae984e46282d
davikawasaki <davishinjik@gmail.com>
2017-01-23 15:44:19 -0200

    Setting up starting project with Angular, Bower, HTTP server

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+var testingAngularApp = angular.module('testingAngularApp', []);
+
+testingAngularApp.controller('testingAngularCtrl', function($rootScope) {
+    var vm = this;
+
+    // Data
+    vm.title="Testing AngularJS Applications";
+
+    // Methods
+
+    ////////////////////////
+
+
+});
